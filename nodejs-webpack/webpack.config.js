eb1d96f6c84a147748255f2e37d8536b69e7ab2d
cinling_ideapad <281093509@qq.com>
2018-08-24 23:09:31 +0800

    提交一个可以成功 webpack 打包的版本

---
+++ nodejs-webpack/webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, './dist')
+    }
+};
+
+module.exports = config;
