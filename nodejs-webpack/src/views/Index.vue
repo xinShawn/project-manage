34a764070efbe8d981dacff57d79cb39d4de626f
cinling_ideapad <281093509@qq.com>
2018-08-26 19:42:56 +0800

    添加 vuex，处理好基本的页面

--- nodejs-webpack/src/views/Index.vue
+++ nodejs-webpack/src/views/Index.vue
@@ -1,45 +1,15 @@
 <template>
-    <main>
-      <el-row :gutter="20">
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-      </el-row>
-    </main>
+  <main>
+    欢迎使用项目管理系统
+  </main>
 </template>
 
 <script>
-export default {
-  name: 'Index'
-}
+  export default {
+    name: 'Index'
+  }
 </script>
 
 <style scoped>
-  .el-row {
-    margin-bottom: 20px;
-  &:last-child {
-     margin-bottom: 0;
-   }
-  }
-  .el-col {
-    border-radius: 4px;
-  }
-  .bg-purple-dark {
-    background: #99a9bf;
-  }
-  .bg-purple {
-    background: #d3dce6;
-  }
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
-  .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
-  }
-  .row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
-  }
+
 </style>
