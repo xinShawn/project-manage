c5a78f1e8b9e3631edf977e1e26ca88008b5950b
cinling <281093509@qq.com>
2018-09-08 08:43:28 +0800

    添加任务相关的数据表以及对应的表模型

---
+++ php-project-manage/models/db/Mission.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace app\models\db;
+
+/**
+ * This is the model class for table "mission".
+ *
+ * @property int $id [int(10) unsigned]
+ * @property int $priority_id [int(11) unsigned]  优先级id
+ * @property string $title [varchar(255)]  任务标题
+ * @property string $content 任务详情
+ * @property bool $status [tinyint(3)]  任务状态。0:未开始 10:进行中 20:暂停 30:已完成 40:已关闭 -10:已取消
+ * @property int $end_time [int(11) unsigned]  截止时间戳
+ * @property int $finish_user_id [int(11) unsigned]  完成人id
+ * @property int $create_user_id [int(11) unsigned]  创建人id
+ * @property int $last_user_id [int(11) unsigned]  上次修改人id
+ * @property int $update_time [int(11) unsigned]  上次更新的时间戳
+ * @property int $create_time [int(11) unsigned]  创建时间
+ */
+class Mission extends BaseDBModel
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'mission';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['priority_id', 'title', 'content', 'create_user_id', 'last_user_id', 'update_time', 'create_time'], 'required'],
+            [['priority_id', 'status', 'end_time', 'finish_user_id', 'create_user_id', 'last_user_id', 'update_time', 'create_time'], 'integer'],
+            [['content'], 'string'],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'priority_id' => 'Priority ID',
+            'title' => 'Title',
+            'content' => 'Content',
+            'status' => 'Status',
+            'end_time' => 'End Time',
+            'finish_user_id' => 'Finish User ID',
+            'create_user_id' => 'Create User ID',
+            'last_user_id' => 'Last User ID',
+            'update_time' => 'Update Time',
+            'create_time' => 'Create Time',
+        ];
+    }
+}
