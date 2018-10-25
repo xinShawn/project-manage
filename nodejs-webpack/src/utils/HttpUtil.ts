6c3e90e03ac213aad64e1951f1495186d589d54c
cinling <281093509@qq.com>
2018-10-26 00:40:44 +0800

    调整 http 请求。默认超时从 5 秒设为 60 秒。删除没用的代码

--- nodejs-webpack/src/utils/HttpUtil.ts
+++ nodejs-webpack/src/utils/HttpUtil.ts
@@ -22,7 +22,7 @@ export default class HttpUtil {
    * @param errorCallback 错误回调
    * @param timeoutMS 超时：毫秒
    */
-  public static axiosPost(url: string, params: object = {}, succCallback: SuccCallback = undefined, errorCallback: Function = undefined, timeoutMS: number = 5000) {
+  public static axiosPost(url: string, params: object = {}, succCallback: SuccCallback = undefined, errorCallback: Function = undefined, timeoutMS: number = 60000) {
 
     try {
       axios.post(HttpUtil.getBaseUrl() + url, params, {
@@ -53,77 +53,10 @@ export default class HttpUtil {
   }
 
   /**
-   * 发送 post 请求。使用原生js的方法(XMLHttpRequest)
-   * @deprecated 推荐使用 axiosPost 代替
-   * @see axiosPost()
-   * @author Cinling
-   * @version 相对比较稳定的版本，但是功能不够齐全
-   * @param relativeUrl 请求相对路径
-   * @param params 请求参数（json object格式）
-   * @param successCallback 成功回调
-   * @param errorCallback 失败回调
-   * @param timeoutMS 超时毫秒数
-   */
-  public static xmlHttpRequestPost(relativeUrl: string, params: object, successCallback: SuccCallback, errorCallback: Function, timeoutMS: number) {
-    if (relativeUrl.indexOf("/", 0) !== 0) {
-      relativeUrl = "/" + relativeUrl;
-    }
-    let xmlHttp = new XMLHttpRequest();
-
-    xmlHttp.responseType = "text";
-    xmlHttp.onreadystatechange = function () {
-
-      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-        let apiReturn = ApiReturnModel.initByXmlResponse(xmlHttp.response);
-        successCallback(apiReturn);
-      } else if (xmlHttp.readyState == 4) {
-        errorCallback({
-          status: xmlHttp.status,
-          response: xmlHttp.response
-        });
-      }
-    };
-
-    xmlHttp.open("post", HttpUtil.getBaseUrl() + relativeUrl, true);
-    xmlHttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-
-    // 发送数据
-    let sendData = HttpUtil.ObjectToPostStr(params);
-    xmlHttp.send(sendData);
-
-    setTimeout(function() {
-      if (xmlHttp.readyState != 4) {
-        xmlHttp.abort();
-        errorCallback("request timeout!");
-      }
-    }, timeoutMS);
-  }
-
-  /**
    * 获取请求的基本路径
    */
   public static getBaseUrl(): string {
     return (process.env.NODE_ENV === 'development') ? '/api' : ''
-  }
-
-  /**
-   * 把对象转为 post 请求的字符串
-   * @param object
-   * @return {string}
-   */
-  private static ObjectToPostStr(object: object) {
-    let postStr = "";
-
-    for (let key in object) {
-      if (postStr.length != 0) {
-        postStr += "&";
-      }
-      let value = object[key];
-
-      postStr += key + "=" + value;
-    }
-
-    return postStr;
   }
 }
 
